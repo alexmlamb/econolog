e14654d52f5250873154ad8b18adbd7569516739
Alex Lamb <alex6200@gmail.com>
2016-03-20 11:37:16 -0400

    Update econolog.py

--- econolog/econolog.py
+++ econolog/econolog.py
@@ -10,10 +10,9 @@ class econolog:
         message: the message to print.  If the message is a string, print it directly.  If it's a dictionary, use a pretty printer.  
         label: a label that is never printed, but is saved in the log file and makes it easier to handle in a structured way.  
         level: Gives the messages a "hierarchy".  level = 0 is the top, level = 1 is more detailed, level = 2 is more detailed
-        and so on.  
+        and so on.  level = -1 means that the message isn't printed (but is still in the log!)
         p: If this is given, the message is printed with probability p (but is always saved to the log).  
         This provides a convenient way of outlining lots of results (like a list of samples where we only need to print a few).  
-        
     '''
 
     def log(self, message, label=None, level = None, p = 1.0):
@@ -29,5 +28,12 @@ class econolog:
     '''
     Prints a summary of all logs seen so far up to a certain level.  
     '''
-    #def exhale(level):
+    def exhale(level):
+        pass
+
+    '''
+    Prints all messages received with the given label.  
+    '''
+    def print_label(label):
+        pass
 
