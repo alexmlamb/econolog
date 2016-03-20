8665d6a4b2faadaba4012785bbe0d8ab2b6422d8
Alex Lamb <alex6200@gmail.com>
2016-03-20 11:25:10 -0400

    Update econolog.py

--- econolog/econolog.py
+++ econolog/econolog.py
@@ -7,20 +7,27 @@ class econolog:
         self.last_level = 0
 
     '''
-        The level refers to how far the message is tabbed.  
+        message: the message to print.  If the message is a string, print it directly.  If it's a dictionary, use a pretty printer.  
+        label: a label that is never printed, but is saved in the log file and makes it easier to handle in a structured way.  
+        level: Gives the messages a "hierarchy".  level = 0 is the top, level = 1 is more detailed, level = 2 is more detailed
+        and so on.  
+        p: If this is given, the message is printed with probability p (but is always saved to the log).  
+        This provides a convenient way of outlining lots of results (like a list of samples where we only need to print a few).  
+        
     '''
 
-    def log(self, message, label, level = None):
+    def log(self, message, label=None, level = None, p = 1.0):
 
         if level == None:
-            level = self.last_level
+            level = 0
 
         assert type(level) is int
         self.last_level = level
         print ("\t" * level) + message
 
 
-
-
-
+    '''
+    Prints a summary of all logs seen so far up to a certain level.  
+    '''
+    #def exhale(level):
 
